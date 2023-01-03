4b1a4b689618d33dd460213d58b90483a9afd6ac
byrachel <byrachel@gmail.com>
2023-01-03 16:57:52 +0100

    try catch

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -7,29 +7,41 @@ const postsController = {
         if(!title || !content || !author || ! date) {
             res.status(422).json({message: "All fields are required."})
         };
-        const db = dbLib.getDB();
-        const collection = db.collection('posts'); 
-        await collection.insertOne({
-            title: title,
-            content: content,
-            author: author,
-            date: date
-        });
-        res.status(201).json({message: "Great ! The post is created."});
+        try {
+            const db = dbLib.getDB();
+            const collection = db.collection('posts'); 
+            await collection.insertOne({
+                title: title,
+                content: content,
+                author: author,
+                date: date
+            });
+            res.status(201).json({message: "Great ! The post is created."});
+        } catch(e) {
+            res.status(500).json({ message: "An error occured. Post not created." });
+        }
     },
     getPosts: async (req,res) => {
-        const db = dbLib.getDB();
-        const data = await db.collection('posts').find().toArray();
-        res.status(200).json({ posts: data });
+        try {
+            const db = dbLib.getDB();
+            const data = await db.collection('posts').find().toArray();
+            res.status(200).json({ posts: data });
+        } catch(e) {
+            res.status(500).json({ message: "An error occured. Posts not found." });
+        }
     },
     getPostById : async(req, res) => {
         const id = req.params.id;
         if(!id) {
             res.status(422).json({ message: "ID is required." });
         }
-        const db = dbLib.getDB();
-        const post = await db.collection('posts').findOne({_id: mongo.ObjectId(id)});
-        res.status(200).json({ post: post });
+        try {
+            const db = dbLib.getDB();
+            const post = await db.collection('posts').findOne({_id: mongo.ObjectId(id)});
+            res.status(200).json({ post: post });
+        } catch(e) {
+            res.status(422).json({ message: "Post not found." });
+        }
     },
     deletePost : async(req, res) => {
         const id = req.params.id;
@@ -41,7 +53,7 @@ const postsController = {
             await db.collection('posts').deleteOne({_id: mongo.ObjectId(id)});
             res.status(200).json({ message: 'Post is deleted !' });
         } catch(e) {
-            res.status(422).json({ message: "ID is required." });
+            res.status(422).json({ message: "Post not found." });
         }
     },
 }
